eafc2ca3968aa09b1773bd1a6594dce316477c3c
Aaron <walte774@morris.umn.edu>
2020-09-30 16:19:59 -0500

    Add some classes

---
+++ src/BlockCipherModes.java
@@ -0,0 +1,11 @@
+public class BlockCipherModes {
+
+    public static void encrypt(String plaintext, String key, String initializationVector, int mode) {
+
+    }
+
+    public static void decrypt(String ciphertext, String key, String initializationVector, int mode) {
+
+    }
+
+}
---
+++ src/sevenBitASCII.java
@@ -0,0 +1,8 @@
+public class sevenBitASCII {
+
+    private int[] bits = {0,0,0,0,0,0,0};
+
+    public sevenBitASCII(char c) {
+
+    }
+}
