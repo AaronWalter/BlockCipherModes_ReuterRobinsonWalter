ca046c27467b12b1eace47c08b7a4956241f12c2
Aaron <walte774@morris.umn.edu>
2020-10-04 19:45:23 -0500

    Implement file reading with scanner

--- src/BlockCipherModes.java
+++ src/BlockCipherModes.java
@@ -1,10 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class BlockCipherModes {
 
-    public static void encrypt(String plaintext, String key, String initializationVector, int mode) {
+    public static void main(String[] args) {
+        String inFileName = args[0];
+
+        try {
+            File toRead = new File(inFileName);
+            Scanner sc = new Scanner(toRead);
+            String iv = sc.nextLine();
+            String key = sc.nextLine();
+            String plaintext = "";
+            while(sc.hasNext()) {
+                plaintext += sc.next();
+            }
+            System.out.println(iv);
+            System.out.println(key);
+            System.out.println(plaintext);
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void encrypt(String plaintext, String key, String initializationVector, int mode) {
 
     }
 
-    public static void decrypt(String ciphertext, String key, String initializationVector, int mode) {
+    private static void decrypt(String ciphertext, String key, String initializationVector, int mode) {
 
     }
 
